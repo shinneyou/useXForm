d5ee4a7d2ab21ce5417af52252f1858f337fd3a5
shinneyou <shinne_you@163.com>
2022-08-25 18:23:20 +0800

    feat: add api

---
+++ src/demo/index.js
@@ -0,0 +1,9 @@
+ReactDOM.render(
+  <div>
+    <antd.Button type="primary">Primary</antd.Button>
+    <antd.Button>Default</antd.Button>
+    <antd.Button type="dashed">Dashed</antd.Button>
+    <antd.Button type="danger">Danger</antd.Button>
+  </div>,
+  document.getElementById("app")
+);
